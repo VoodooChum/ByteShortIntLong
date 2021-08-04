7d183b381e5e8fb9a4a92ed1fcd5b7f24a385c2f
brimill <brian.miller.ext@sodexo.com>
2021-08-04 17:48:33 -0500

    Tested Min and Max

---
+++ src/udemy/primatives/Main.java
@@ -0,0 +1,12 @@
+package udemy.primatives;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int myValue = 10000;
+	int myMinIntValue = Integer.MIN_VALUE;
+	int  myMaxIntValue = 2_147_483_647;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+    }
+}
